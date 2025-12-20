21aeab28bd0c87991dc43814def91b2b5a69e980
rasum13 <rasum.subedi13@gmail.com>
2025-12-20 20:47:41 +0545

    move disclaimer to bottom of page

--- frontend/src/components/Disclaimer.jsx
+++ frontend/src/components/Disclaimer.jsx
@@ -1,8 +1,8 @@
 import { TriangleAlert } from "lucide-react"
 
-const Disclaimer = ({ title, message }) => {
+const Disclaimer = ({ title, message, className }) => {
   return (
-    <div className="bg-yellow-light rounded-md p-4 border-2 border-yellow text-yellow max-w-200">
+    <div className={`bg-yellow-light rounded-md p-4 border border-yellow text-yellow absolute bottom-28 lg:bottom-4 right-4 left-4 lg:left-80 ${className}`}>
       <h3 className="text-base flex mb-2"><TriangleAlert />&nbsp;{title}</h3>
       <p className="text-sm">{message}</p>
     </div>
